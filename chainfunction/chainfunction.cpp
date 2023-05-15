6fd52b820b31dde65825abfec64cd1b6e5f2142e
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-15 13:09:06 +0700

    tambahan int main

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -16,3 +16,11 @@ public:
 	}
 
 } bukunya;
+
+int main()
+{
+	//	bukunya.setjudul("Matematika");
+	//	cout << bukunya.getjudul();
+	cout << bukunya.setJudul("Matematika").getJudul();	// chain function calls
+	return 0;
+}
